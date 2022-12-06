20e16154c5d106c4a548d770f946cbf7cbdd422d
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-06 22:10:11 +0300

    task 1.1.5

--- 1. Основы реляционной модели и SQL/1.1 Отношение (таблица)/1.sql
+++ 1. Основы реляционной модели и SQL/1.1 Отношение (таблица)/1.sql
@@ -1,6 +1,6 @@
-/*Сформулируйте SQL запрос для создания таблицы book,
-занесите  его в окно кода (расположено ниже)  и отправьте на
-проверку (кнопка Отправить). Структура таблицы book:*/
-
-CREATE TABLE book(book_id INT PRIMARY KEY AUTO_INCREMENT, 
-title VARCHAR(50), author VARCHAR(30), price DECIMAL(8, 2), amount INT);
+CREATE TABLE book(                              /*Создать таблицу с именем book*/
+    book_id INT PRIMARY KEY AUTO_INCREMENT,     /*Столбец book_id целое число атоматическое заполнение*/
+    title VARCHAR(50),                          /*Столбец title строка до 50 символов*/
+    author VARCHAR(30),                         /*Столбец author строка до 30 символов*/
+    price DECIMAL(8, 2),                        /*Столбец price число до 8 символов 2 из них дробь*/
+    amount INT);                                /*Столбец amount целое число*/
