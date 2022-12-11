5c37172176e45d384f3153546afa66a86b5ecb3d
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-11 20:31:16 +0300

    task 1.5.6.

--- 1. Основы реляционной модели и SQL/1.5 Запросы корректировки данных/6.sql
+++ 1. Основы реляционной модели и SQL/1.5 Запросы корректировки данных/6.sql
@@ -2,5 +2,9 @@
 для покупателя в столбце buy таким образом, 
 чтобы оно не превышало допустимый остаток в 
 столбце amount. А цену тех книг, которые покупатель не заказывал, снизить на 10%.*/
-UPDATE book SET buy = IF(amount - buy < 0, amount, buy);
-UPDATE book SET price = price * 0.9 WHERE buy = 0;
+UPDATE book SET buy = IF(amount - buy < 0, amount, buy);    /* В таблице book изменить значение столбца buy */
+UPDATE book SET price = price * 0.9 WHERE buy = 0;          /* В таблице book изменить значение столбца price */
+
+
+update book set buy = if(buy > amount, amount, buy),        /* В таблице book изменить значение столбца buy */
+                price = if(buy = 0, price * 0.9, price);    /* изменить значение столбца price */
