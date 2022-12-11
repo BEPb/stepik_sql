95191b92cf3efa29005771eaec95e03944c1cd32
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-11 20:14:33 +0300

    task 1.5.4.

--- 1. Основы реляционной модели и SQL/1.5 Запросы корректировки данных/4.sql
+++ 1. Основы реляционной модели и SQL/1.5 Запросы корректировки данных/4.sql
@@ -1,5 +1,4 @@
-/*Занести из таблицы supply в таблицу book только те книги, авторов которых нет в  book.*/
-INSERT INTO book (title, author, price, amount) 
-SELECT title, author, price, amount 
-FROM supply 
-WHERE author NOT IN (SELECT author FROM book);
+INSERT INTO book (title, author, price, amount)         /* вставить в таблицу */
+SELECT title, author, price, amount                     /* выбрать столбцы */
+FROM supply                                             /* из таблицы */
+WHERE author NOT IN (SELECT author FROM book);          /* где автор не в списке авторов таблицы */
