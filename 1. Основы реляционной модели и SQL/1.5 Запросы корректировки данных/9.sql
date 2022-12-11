bf73e19da16b8b013b3d22ea2cbea307e86f593f
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-11 21:04:25 +0300

    task 1.5.9.

--- 1. Основы реляционной модели и SQL/1.5 Запросы корректировки данных/9.sql
+++ 1. Основы реляционной модели и SQL/1.5 Запросы корректировки данных/9.sql
@@ -1,9 +1,5 @@
-/*Создать таблицу заказ (ordering), куда включить авторов и 
-названия тех книг, количество экземпляров которых в таблице 
-book меньше среднего количества экземпляров книг в таблице
-book. В таблицу включить столбец   amount, в котором для всех 
-книг указать одинаковое значение - среднее количество экземпляров книг в таблице book.*/
-CREATE TABLE ordering AS SELECT author, title, (SELECT ROUND(AVG(amount)) FROM book) AS amount 
-FROM book 
-WHERE amount < (SELECT AVG(amount) FROM book);
-SELECT * FROM ordering;
+CREATE TABLE ordering                                   /*Создать таблицу заказ (ordering) */
+    AS SELECT author, title,                            /* столбцы */
+    (SELECT ROUND(AVG(amount)) FROM book) AS amount     /* столбец amount как среднее значение из таблицы book*/
+        FROM book                                       /* из таблицы book */
+        WHERE amount < (SELECT AVG(amount) FROM book);  /* где количество меньше среднего (убираем лишние строки) */
