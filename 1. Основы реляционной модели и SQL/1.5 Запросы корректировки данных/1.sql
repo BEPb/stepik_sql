e2f6dda80fe61fe76815f1f5bdf780442c337cb5
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-11 19:47:10 +0300

    task 1.5.1.

--- 1. Основы реляционной модели и SQL/1.5 Запросы корректировки данных/1.sql
+++ 1. Основы реляционной модели и SQL/1.5 Запросы корректировки данных/1.sql
@@ -1,2 +1,6 @@
-/*Создать таблицу поставка (supply), которая имеет ту же структуру, что и таблица book.*/
-CREATE TABLE supply (supply_id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(50), author VARCHAR(30), price DECIMAL(8, 2), amount INT);
+CREATE TABLE supply                                 /* Создать таблицу поставка (supply)  */
+    (supply_id INT PRIMARY KEY AUTO_INCREMENT,      /* столбец supply_id целое число автозаполнение */
+    title VARCHAR(50),                              /* столбец title текст до 50 символов */
+    author VARCHAR(30),                             /* столбец author текст до 30 символов */
+    price DECIMAL(8, 2),                            /* столбец price дробное число */
+    amount INT);                                    /* столбец amount целое число */
