fe01205c083d1110e41ba03cba4a103935be9f77
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-11 12:28:30 +0300

    task 1.2.4.

--- 1. Основы реляционной модели и SQL/1.2 Выборка данных/4.sql
+++ 1. Основы реляционной модели и SQL/1.2 Выборка данных/4.sql
@@ -1,4 +1,4 @@
 SELECT                          /* вывести  */
     title, amount,              /* столбцы title, amount */
     1.65 * amount AS pack       /* столбец amount умноженный на 1.65 под псевдонином pack */
-FROM book;                      /* из таблицы book */
+FROM book;                      /* из таблицы book  */
