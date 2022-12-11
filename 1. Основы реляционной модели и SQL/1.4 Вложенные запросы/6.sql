bf2286de3f790534170d25bbd32b48f69454e2a2
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-11 19:25:57 +0300

    task 1.4.6.

--- 1. Основы реляционной модели и SQL/1.4 Вложенные запросы/6.sql
+++ 1. Основы реляционной модели и SQL/1.4 Вложенные запросы/6.sql
@@ -1,2 +1,6 @@
-/**/
-SELECT author, title, price, amount, (price * amount) AS income_percent FROM book;
+SELECT                                                  /* выбрать информацию */
+    title, author, amount,                              /* столбцы */
+    (SELECT MAX(amount) FROM book) - amount AS Заказ    /* столбец Заказ вычисляемый как разницы макс. значения от количества */
+    FROM book                                           /* из таблицы */
+    WHERE amount <> (SELECT MAX(amount) FROM book);     /* где количество не равно максимальному */
+
