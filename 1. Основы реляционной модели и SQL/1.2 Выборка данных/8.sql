1b44924237790310c10ae7a775e1528cc58c3752
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-11 12:24:35 +0300

    task 1.2.8.

--- 1. Основы реляционной модели и SQL/1.2 Выборка данных/8.sql
+++ 1. Основы реляционной модели и SQL/1.2 Выборка данных/8.sql
@@ -1,5 +1,4 @@
-/*Вывести название, автора,  цену  и количество всех книг, 
-цена которых меньше 500 или больше 600, а стоимость всех 
-экземпляров этих книг больше или равна 5000.*/
-SELECT title, author, price, amount FROM book WHERE 
-(price < 500 OR price > 600) AND price * amount >=5000;
+SELECT                                  /* вывести */
+    title, author, price, amount        /* столбцы */
+    FROM book                           /* из таблицы */
+    WHERE (price < 500 OR price > 600) AND price * amount > 5000;  /* соответствующие условиям */
