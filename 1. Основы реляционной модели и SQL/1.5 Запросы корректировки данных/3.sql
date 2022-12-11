0100cf8171461d734f132684b2e5db5223085e7b
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-11 20:09:49 +0300

    task 1.5.3.

--- 1. Основы реляционной модели и SQL/1.5 Запросы корректировки данных/3.sql
+++ 1. Основы реляционной модели и SQL/1.5 Запросы корректировки данных/3.sql
@@ -1,6 +1,4 @@
-/*Добавить из таблицы supply в таблицу book, все книги, 
-кроме книг, написанных Булгаковым М.А. и Достоевским Ф.М.*/
-INSERT INTO book (title, author, price, amount) 
-SELECT title, author, price, amount 
-FROM supply 
-WHERE author NOT IN ("Достоевский Ф.М.", "Булгаков М.А.");
+INSERT INTO book (title, author, price, amount)             /*Добавить в таблицу book */
+SELECT title, author, price, amount                         /*выбрать столбцы */
+FROM supply                                                 /*из таблицы */
+WHERE author NOT IN ("Достоевский Ф.М.", "Булгаков М.А.");  /*где автор не из списка */
