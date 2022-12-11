6a2b3217b8b870ce1be38153318376f4c9302239
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-11 17:43:13 +0300

    task 1.4.1.

--- 1. Основы реляционной модели и SQL/1.4 Вложенные запросы/1.sql
+++ 1. Основы реляционной модели и SQL/1.4 Вложенные запросы/1.sql
@@ -1,8 +1,5 @@
-/*Вывести информацию (автора, название и цену) о  книгах, 
-цены которых меньше или равны средней цене книг на складе. 
-Информацию вывести в отсортированном по убыванию цены виде. 
-Среднее вычислить как среднее по цене книги.*/
- SELECT author, title, price 
- FROM book 
- WHERE price <= (SELECT AVG(price) FROM book) 
- ORDER BY price DESC;
+SELECT                              /* вывести */
+    author, title, price            /* столбцы */
+    FROM book                       /* из таблицы */
+    WHERE price<= (SELECT AVG(price) FROM book)     /* где цена меньше или равна средней цене из таблицы*/
+    ORDER BY price DESC             /* отсортировать по цене в обратной последовательности*/
