4930cf36c3724efed17767282e016f2f924a5918
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-13 22:09:19 +0300

    task 1.7.1.

--- 1. Основы реляционной модели и SQL/1.7 Таблица Нарушения ПДД, запросы корректировки/1.sql
+++ 1. Основы реляционной модели и SQL/1.7 Таблица Нарушения ПДД, запросы корректировки/1.sql
@@ -1,5 +1,9 @@
-/*Создать таблицу fine*/
-CREATE TABLE fine 
-(fine_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30), number_plate VARCHAR(6), 
-violation VARCHAR(50), sum_fine DECIMAL(8, 2), date_violation DATE, date_payment DATE);
+CREATE TABLE fine                               /*Создать таблицу fine*/
+    (fine_id INT PRIMARY KEY AUTO_INCREMENT,    /* столбец  автозаполнение */
+    name VARCHAR(30),                           /* столбец строка до 30 символов */
+    number_plate VARCHAR(6),                    /* столбец строка до 6 символов */
+    violation VARCHAR(50),                      /* столбец строка до 50 символов */
+    sum_fine DECIMAL(8, 2),                     /* столбец дробное число */
+    date_violation DATE,                        /* столбец дата */
+    date_payment DATE);                         /* столбец дата */
 
