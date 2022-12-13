85bf3945a00f129caafe735021b0acc8754fe636
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-13 20:18:33 +0300

    task 1.6.1.

--- 1. Основы реляционной модели и SQL/1.6 Таблица Командировки, запросы на выборку/1.sql
+++ 1. Основы реляционной модели и SQL/1.6 Таблица Командировки, запросы на выборку/1.sql
@@ -1,8 +1,5 @@
-/*Вывести из таблицы trip информацию о командировках тех сотрудников, 
-фамилия которых заканчивается на букву «а», в отсортированном по 
-убыванию даты последнего дня командировки виде. В результат 
-включить столбцы name, city, per_diem, date_first, date_last.*/
-SELECT name, city, per_diem, date_first, date_last 
-FROM trip 
-WHERE name LIKE "%а _._." 
-ORDER BY date_last DESC;
+SELECT                                              /* выбрать данные */
+    name, city, per_diem, date_first, date_last     /* столбцы */
+    FROM trip                                       /* из таблицы */
+    WHERE name LIKE "%а _._."                       /* где имя заканчивается на а */
+    ORDER BY date_last DESC;                        /* отсортированном по убыванию даты последнего дня командировки */
