8b424cb109390e58e677ac9178aefb4b89b46afa
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-17 18:40:20 +0300

    task 2.2.7.

--- 2. Запросы SQL к связанным таблицам/2.2 Запросы на выборку, соединение таблиц/7.sql
+++ 2. Запросы SQL к связанным таблицам/2.2 Запросы на выборку, соединение таблиц/7.sql
@@ -1,10 +1,16 @@
-/*Вывести информацию о книгах, написанных в самых популярных жанрах, 
-в отсортированном в алфавитном порядке по названию книг виде.*/
-SELECT title, name_author, name_genre, price, amount 
-FROM author 
-	INNER JOIN book ON author.author_id = book.author_id 
-    INNER JOIN genre ON genre.genre_id = book.genre_id 
-WHERE book.genre_id IN (
-    SELECT genre_id FROM book GROUP BY genre_id HAVING SUM(amount) = 
-(SELECT MAX(sums) AS max_sum FROM (SELECT SUM(amount) AS sums FROM book GROUP BY genre_id) buff)) 
-ORDER BY title; 
+SELECT                                                      /* выбрать данные */
+    title, name_author, name_genre, price, amount           /* столбцы */
+FROM author                                                 /* из таблицы */
+	INNER JOIN book ON author.author_id = book.author_id    /* соединить с таблицей */
+    INNER JOIN genre ON genre.genre_id = book.genre_id      /* соединить с таблицей */
+WHERE book.genre_id IN (                                    /* где жанр книги */
+    SELECT genre_id                                         /* выбрать данные */
+    FROM book                                               /* из таблицы */
+    GROUP BY genre_id                                       /* сгруппировать по */
+    HAVING SUM(amount) =                                    /* имеющий сумму количества равную */
+        (SELECT MAX(sums) AS max_sum                        /* выбрать данные */
+        FROM                                                /* из */
+        (SELECT SUM(amount) AS sums                         /* выбрать сумму количества */
+        FROM book                                           /* из таблицы */
+        GROUP BY genre_id) buff))                           /* сгруппировать */
+ORDER BY title;                                             /* отсортировать по названию */
