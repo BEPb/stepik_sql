12da91b449231156299e1c95c86bbfda3e028b0b
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-17 17:01:01 +0300

    task 2.2.5.

--- 2. Запросы SQL к связанным таблицам/2.2 Запросы на выборку, соединение таблиц/5.sql
+++ 2. Запросы SQL к связанным таблицам/2.2 Запросы на выборку, соединение таблиц/5.sql
@@ -1,9 +1,14 @@
-/*Посчитать количество экземпляров  книг каждого автора на складе.  
-Вывести тех авторов,  количество книг которых меньше 10, в 
-отсортированном по возрастанию количества виде. Последний столбец назвать Количество.*/
-SELECT name_author, SUM(amount) AS Количество 
-FROM author 
-	LEFT JOIN book ON author.author_id = book.author_id 
-GROUP BY name_author 
-HAVING SUM(amount) < 10 OR SUM(amount) IS NULL 
-ORDER BY Количество;
+SELECT                          /* выбрать данные */
+    name_author,                /* столбец */
+    SUM(amount) AS Количество   /* столбец сумма количества */
+FROM                            /* из*/
+    author                      /* таблицы */
+    LEFT JOIN book              /* соединенной */
+    ON author.author_id = book.author_id     /* при условии */
+GROUP BY                        /* сгруппированной */
+    name_author                 /* по авторам */
+HAVING                          /* имеющих */
+    Количество < 10             /* условие 1 */
+    OR Количество is NULL       /* или условие 2 */
+ORDER BY                        /* отсортированном */
+    Количество;                 /* по количеству */
