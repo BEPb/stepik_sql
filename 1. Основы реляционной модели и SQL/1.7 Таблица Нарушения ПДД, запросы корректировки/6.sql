6be196606984961ea327ea5c78e9d774555f64a5
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-17 10:37:20 +0300

    task 1.7.6.

--- 1. Основы реляционной модели и SQL/1.7 Таблица Нарушения ПДД, запросы корректировки/6.sql
+++ 1. Основы реляционной модели и SQL/1.7 Таблица Нарушения ПДД, запросы корректировки/6.sql
@@ -1,10 +1,5 @@
-/*Необходимо:
-в таблицу fine занести дату оплаты соответствующего штрафа из таблицы payment; 
-уменьшить начисленный штраф в таблице fine в два раза  (только для новых штрафов, 
-дата оплаты которых занесена в payment) , если оплата произведена не более, 
-чем за 20 дней со дня нарушения.*/
-UPDATE fine f, payment p 
-SET f.date_payment = p.date_payment, f.sum_fine = 
-IF((DATEDIFF(p.date_payment, f.date_violation) + 1) <= 20, f.sum_fine / 2, f.sum_fine) 
-WHERE f.name = p.name AND f.number_plate = p.number_plate AND f.violation = p.violation AND f.date_violation = p.date_violation;
+UPDATE fine f, payment p                        /* обновить значение таблицы fine из таблицы payment */
+SET    f.date_payment = p.date_payment,         /* занести дату оплаты соответствующего штрафа из таблицы payment */
+       f.sum_fine = IF(DATEDIFF(p.date_payment, f.date_violation) <= 20, f.sum_fine / 2, f.sum_fine)                        /* уменьшить начисленный штраф в таблице fine в два раза, только для новых штрафов */
+WHERE (f.name, f.number_plate, f.violation, f.date_violation) = (p.name, p.number_plate, p.violation, p.date_violation);    /* где данные из двух таблиц соответствуют одному штрафу */
 
