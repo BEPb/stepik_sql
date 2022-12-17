759e036cf3b1d54943c40efdba137a1f50e1729f
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-17 12:03:31 +0300

    task 2.1.7.

--- 2. Запросы SQL к связанным таблицам/2.1 Связи между таблицами/3.sql
+++ 2. Запросы SQL к связанным таблицам/2.1 Связи между таблицами/3.sql
@@ -1,6 +1,12 @@
-/*Перепишите запрос на создание таблицы book , чтобы ее структура 
-соответствовала рисунку ниже  (Связать таблицы book и genre) . 
-В качестве главной таблицы для описания поля  genre_idиспользовать таблицу genre*/
-CREATE TABLE book2 (book_id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(50), author_id INT, 
-genre_id INT, price DECIMAL(8, 2), amount INT, FOREIGN KEY (author_id) 
-REFERENCES author (author_id), FOREIGN KEY (genre_id) REFERENCES genre (genre_id));
+CREATE TABLE book (                                             /* создать таблицу */
+      book_id INT PRIMARY KEY AUTO_INCREMENT,                   /* столбец целое число */
+      title VARCHAR(50),                                        /* столбец строка 50 символов */
+      author_id INT NOT NULL,                                   /* столбец целое число не ноль */
+      genre_id INT,                                             /* столбец целое число */
+      price DECIMAL(8,2),                                       /* столбец дробное число */
+      amount INT,                                               /* столбец целое число */
+      FOREIGN KEY (author_id)  REFERENCES author (author_id),   /* указываем внешний ключ */
+      FOREIGN KEY (genre_id)  REFERENCES genre (genre_id)       /* указываем внешний ключ */
+);
+
+DESCRIBE book;                                                  /* информации о столбцах таблицы (тип значений, является ключом или нет и т.д.) */
