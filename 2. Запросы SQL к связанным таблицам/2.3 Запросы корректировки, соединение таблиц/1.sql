7aa62bfdffa56b5ff5d10916401ba4880fb11716
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-18 15:45:40 +0300

    task 2.3.1.

--- 2. Запросы SQL к связанным таблицам/2.3 Запросы корректировки, соединение таблиц/1.sql
+++ 2. Запросы SQL к связанным таблицам/2.3 Запросы корректировки, соединение таблиц/1.sql
@@ -1,14 +1,12 @@
-/*Для книг, которые уже есть на складе (в таблице book),
- но по другой цене, чем в поставке (supply),  
- необходимо в таблице book увеличить количество 
- на значение, указанное в поставке,  и пересчитать
- цену. А в таблице  supply обнулить количество этих книг.*/
-UPDATE book 
-	INNER JOIN author ON book.author_id = author.author_id 
-    INNER JOIN supply ON book.title = supply.title AND author.name_author = supply.author 
-SET supply.amount = 0, 
-	book.amount = book.amount + supply.amount, 
-    book.price = ((book.price * book.amount) + (supply.amount * supply.price)) / (book.amount + supply.amount) 
-WHERE book.price <> supply.price;
-SELECT * FROM book;
-SELECT * FROM supply;
+UPDATE book                                                                                 /* обновить таблицу */
+	INNER JOIN author ON book.author_id = author.author_id                                  /* связать с таблицей */
+    INNER JOIN supply ON book.title = supply.title AND author.name_author = supply.author   /* связать с таблицей*/
+SET supply.amount = 0,                                                                      /* внести изменения в столбец*/
+	book.amount = book.amount + supply.amount,                                              /* внести изменения в столбец*/
+    book.price = ((book.price * book.amount) + (supply.amount * supply.price)) / (book.amount + supply.amount)      /* внести изменения в столбец */
+WHERE book.price <> supply.price;                                                           /* где цена не равна */
+SELECT * FROM book;                                                                         /* вывести таблицу */
+SELECT * FROM supply;                                                                       /* вывести таблицу */
+
+
+
