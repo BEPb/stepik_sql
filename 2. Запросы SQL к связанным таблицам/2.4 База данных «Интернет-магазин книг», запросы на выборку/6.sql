d2715414ed5a4350be5fc48ba78f5cd444cf0379
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-24 21:35:46 +0300

    task 2.4.6.

--- 2. Запросы SQL к связанным таблицам/2.4 База данных «Интернет-магазин книг», запросы на выборку/6.sql
+++ 2. Запросы SQL к связанным таблицам/2.4 База данных «Интернет-магазин книг», запросы на выборку/6.sql
@@ -1,5 +1,9 @@
-/*Вывести номера всех оплаченных заказов и даты, когда они были оплачены.*/
-SELECT buy_id, date_step_end 
-FROM step 
-	INNER JOIN buy_step ON step.step_id = buy_step.step_id 
-WHERE step.step_id = 1 AND date_step_end IS NOT NULL;
+SELECT                                                      /* выбрать данные */
+    buy_id, date_step_end                                   /* столбцы */
+FROM step                                                   /* из таблицы */
+	INNER JOIN buy_step ON step.step_id = buy_step.step_id  /* объединив с таблицей где номера заказов равны */
+WHERE                                                       /* где */
+    step.step_id = 1 AND                                    /* значение номера заказа = 1 (оплачено) */
+    date_step_end IS NOT NULL;                              /* дата оплаты указана */
+
+
