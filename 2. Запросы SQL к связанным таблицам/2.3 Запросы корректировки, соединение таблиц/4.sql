6cf40a675b438de1e5990aafaa49f14bae692c41
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-24 19:29:44 +0300

    task 2.3.4.

--- 2. Запросы SQL к связанным таблицам/2.3 Запросы корректировки, соединение таблиц/4.sql
+++ 2. Запросы SQL к связанным таблицам/2.3 Запросы корректировки, соединение таблиц/4.sql
@@ -1,12 +1,21 @@
-/* Занести для книги «Стихотворения и поэмы» Лермонтова жанр «Поэзия», 
-а для книги «Остров сокровищ» Стивенсона - «Приключения». (Использовать два запроса)..*/
-UPDATE book SET genre_id = 
-	(SELECT genre_id 
-    FROM genre 
-    WHERE name_genre = "Поэзия") 
-WHERE title = "Стихотворения и поэмы" AND author_id = 5;
-UPDATE book SET genre_id = 
-	(SELECT genre_id 
-    FROM genre 
-    WHERE name_genre = "Приключения") 
-WHERE title = "Остров сокровищ" AND author_id = 6;
+UPDATE book                                                 /* обновить таблицу */
+INNER JOIN author                                           /* объединенной с таблицей */
+ON book.author_id = author.author_id                        /* где номер автора одинаков */
+SET genre_id =                                              /* столбец жанр */
+	(SELECT genre_id                                        /* выбрать данные столбца */
+    FROM genre                                              /* из таблицы */
+    WHERE name_genre = "Поэзия")                            /* где жанр =, это же значение и внести в таблицу */
+WHERE                                                       /* где */
+    title = "Стихотворения и поэмы" AND                     /* название произведения = */
+    name_author LIKE "%Лермонтов%";                         /* имя автора = */
+
+UPDATE book                                                 /* обновить таблицу */
+INNER JOIN author                                           /* объединенной с таблицей */
+ON book.author_id = author.author_id                        /* где номер автора одинаков */
+SET genre_id =                                              /* столбец жанр */
+	(SELECT genre_id                                        /* выбрать данные столбца */
+    FROM genre                                              /* из таблицы */
+    WHERE name_genre = "Приключения")                       /* где жанр =, это же значение и внести в таблицу */
+WHERE                                                       /* где */
+    title = "Остров сокровищ" AND                           /* название произведения = */
+    name_author LIKE "%Стивенсон%";                         /* имя автора = */
