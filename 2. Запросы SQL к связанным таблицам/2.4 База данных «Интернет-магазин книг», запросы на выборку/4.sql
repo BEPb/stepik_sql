d8d01a09de71ae09a0c63a4f3b643dc6babdc0c9
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-24 21:14:50 +0300

    task 2.4.4.

--- 2. Запросы SQL к связанным таблицам/2.4 База данных «Интернет-магазин книг», запросы на выборку/4.sql
+++ 2. Запросы SQL к связанным таблицам/2.4 База данных «Интернет-магазин книг», запросы на выборку/4.sql
@@ -1,11 +1,13 @@
-/*Посчитать, сколько раз была заказана каждая книга,
- для книги вывести ее автора (нужно посчитать,
- в каком количестве заказов фигурирует каждая книга). 
- Результат отсортировать сначала  по фамилиям авторов,
- а потом по названиям книг. Последний столбец назвать Количество.*/
-SELECT name_author, title, COUNT(buy_book.book_id) AS Количество 
-FROM author 
-	INNER JOIN book ON author.author_id = book.author_id 
-	LEFT JOIN buy_book ON book.book_id = buy_book.book_id 
-GROUP BY name_author, title 
-ORDER BY author.name_author, book.title;
+SELECT                                      /* выбрать данные */
+    author.name_author,                     /* таблица.столбец */
+    book.title,                             /* таблица.столбец */
+    COUNT(buy_book.book_id) AS Количество   /* количество (книг) как псевдоним */
+FROM                                        /* из */
+    book                                    /* таблицы */
+    LEFT JOIN buy_book USING (book_id)      /* левое объединение с таблицей */
+    INNER JOIN author USING (author_id)     /* объединение с таблицей */
+GROUP BY                                    /* сгруппировать */
+    book.book_id                            /* по таблица.столбец */
+ORDER BY                                    /* отсортировать */
+    author.name_author,                     /* по имени автора */
+    book.title;                             /* затем по названию книг */
