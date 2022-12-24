5244fb69bb03c961ec76894dfc0eca151df6fd65
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-24 22:36:27 +0300

    task 2.4.9.

--- 2. Запросы SQL к связанным таблицам/2.4 База данных «Интернет-магазин книг», запросы на выборку/9.sql
+++ 2. Запросы SQL к связанным таблицам/2.4 База данных «Интернет-магазин книг», запросы на выборку/9.sql
@@ -1,18 +1,13 @@
-/*В таблице city для каждого города указано количество дней, 
-за которые заказ может быть доставлен в этот город 
-(рассматривается только этап "Транспортировка"). 
-Для тех заказов, которые прошли этап транспортировки, 
-вывести количество дней за которое заказ реально доставлен 
-в город. А также, если заказ доставлен с опозданием, указать
- количество дней задержки, в противном случае вывести 0.
- Информацию вывести в отсортированном по номеру заказа виде.*/
-SELECT DISTINCT buy_book.buy_id, DATEDIFF(date_step_end, date_step_beg) AS Количество_дней, 
-IF(DATEDIFF(date_step_end, date_step_beg) > city.days_delivery, 
-DATEDIFF(date_step_end, date_step_beg) - city.days_delivery, 0) AS Опоздание 
-FROM city 
-	INNER JOIN client USING (city_id) 
-    INNER JOIN buy USING (client_id) 
-    INNER JOIN buy_book USING (buy_id) 
-    INNER JOIN buy_step USING (buy_id) 
-    INNER JOIN step USING (step_id) 
-WHERE buy_step.step_id = 3 AND DATEDIFF(date_step_end, date_step_beg) IS NOT NULL;
+SELECT                                                              /* выбрать данные */
+    buy_id,                                                         /* столбец */
+    DATEDIFF(date_step_end, date_step_beg) AS Количество_дней,      /* вычислить и назвать столбец */
+    GREATEST(DATEDIFF(date_step_end, date_step_beg) - days_delivery, 0) AS Опоздание    /* вычислить и назвать столбец */
+FROM buy                                                            /* из таблицы */
+    INNER JOIN buy_step USING(buy_id)                               /* объединить с таблицей */
+    INNER JOIN step     USING(step_id)                              /* объединить с таблицей */
+    INNER JOIN client   USING(client_id)                            /* объединить с таблицей */
+    INNER JOIN city     USING(city_id)                              /* объединить с таблицей */
+WHERE                                                               /* где */
+    name_step = 'Транспортировка' AND                               /* условие 1 и*/
+    date_step_end IS NOT NULL                                       /* условие 2 */
+ORDER BY buy_id;                                                    /* отсортировать по номеру заказа */
