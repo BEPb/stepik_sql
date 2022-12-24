0a2fb68148ded598e64f9533095452b7e49bcee2
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-24 19:54:03 +0300

    task 2.3.7.

--- 2. Запросы SQL к связанным таблицам/2.3 Запросы корректировки, соединение таблиц/7.sql
+++ 2. Запросы SQL к связанным таблицам/2.3 Запросы корректировки, соединение таблиц/7.sql
@@ -1,9 +1,4 @@
-/*Удалить всех авторов, которые пишут в жанре "Поэзия". 
-Из таблицы book удалить все книги этих авторов. В запросе
- для отбора авторов использовать полное название жанра, а не его id.*/
-DELETE FROM author USING author 
-	INNER JOIN book ON book.author_id = author.author_id 
-    INNER JOIN genre ON genre.genre_id = book.genre_id 
-WHERE name_genre = "Поэзия";
-SELECT * FROM author;
-SELECT * FROM book;
+DELETE FROM author USING author                             /* удалить из таблицы */
+	INNER JOIN book ON book.author_id = author.author_id    /* используя таблицу объединенную с таблицей где номер автора равен */
+    INNER JOIN genre ON genre.genre_id = book.genre_id      /* объединенную с таблицей где номер жанра равен */
+WHERE name_genre = "Поэзия";                                /* где имя жанра = */
