9763274075512d68b17705f8226dd26ca24e7ca2
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-26 22:24:57 +0300

    task 2.5.9.

--- 2. Запросы SQL к связанным таблицам/2.5 База данных «Интернет-магазин книг», запросы корректировки/9.sql
+++ 2. Запросы SQL к связанным таблицам/2.5 База данных «Интернет-магазин книг», запросы корректировки/9.sql
@@ -1,10 +1,18 @@
-/*Завершить этап «Оплата» для заказа с номером 5, вставив в столбец 
-date_step_end дату 13.04.2020, и начать следующий этап («Упаковка»), 
-задав в столбце date_step_beg для этого этапа ту же дату.*/
-UPDATE buy_step SET date_step_end = "2020-04-13" 
-WHERE buy_id = 5 AND step_id = (SELECT step_id FROM step WHERE name_step = "Оплата");
-
-UPDATE buy_step SET date_step_beg = "2020-04-13" 
-WHERE buy_id = 5 AND step_id = (SELECT step_id + 1 FROM step WHERE name_step = "Оплата");
-
-SELECT * FROM buy_step;
+-- первый запрос
+UPDATE                              /* обновить */
+    buy_step bs                     /* таблицу */
+    JOIN step USING (step_id)       /* объединенную с таблицей по столбцу */
+SET                                 /* внести значения */
+    bs.date_step_end = "2020-04-13" /* в столбец = */
+WHERE                               /* где */
+    bs.buy_id = 5 AND               /* условие 1 */
+    step.name_step = "Оплата";      /* условие 2 */
+-- второй запрос
+UPDATE                              /* обновить */
+    buy_step bs                     /* таблицу */
+    JOIN step USING (step_id)       /* объединенную с таблицей по столбцу */
+SET                                 /* внести значения */
+    bs.date_step_beg = "2020-04-13" /* в столбец = */
+WHERE                               /* где */
+    bs.buy_id = 5 AND               /* условие 1 */
+    step.name_step = "Упаковка";    /* условие 2 */
