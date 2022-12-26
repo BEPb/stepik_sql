d7c6377c3b1eabf0e0d5f7a4d970b24d1b1e440b
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-26 22:09:52 +0300

    task 2.5.8.

--- 2. Запросы SQL к связанным таблицам/2.5 База данных «Интернет-магазин книг», запросы корректировки/8.sql
+++ 2. Запросы SQL к связанным таблицам/2.5 База данных «Интернет-магазин книг», запросы корректировки/8.sql
@@ -1,4 +1,8 @@
-/*В таблицу buy_step занести дату 12.04.2020 выставления счета на оплату заказа с номером 5.*/
-UPDATE buy_step SET date_step_beg = "2020-04-12"
-WHERE buy_id = 5 AND step_id = (SELECT step_id FROM step WHERE name_step = "Оплата");
-SELECT * FROM buy_step;
+UPDATE                                      /* обновить */
+    buy_step                                /* таблицу */
+    JOIN step USING (step_id)               /* объединенную с таблицей */
+SET                                         /* внести данные */
+    buy_step.date_step_beg = '2020-04-12'   /* в столбец значения */
+WHERE                                       /* где */
+    buy_step.buy_id = 5 &&                  /* условие 1 */
+    step.name_step = 'Оплата';              /* условие 2 */
