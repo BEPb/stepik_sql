5593b27f69a37a11f99c7289564f6d93aff0eeff
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-26 21:55:01 +0300

    task 2.5.6.

--- 2. Запросы SQL к связанным таблицам/2.5 База данных «Интернет-магазин книг», запросы корректировки/6.sql
+++ 2. Запросы SQL к связанным таблицам/2.5 База данных «Интернет-магазин книг», запросы корректировки/6.sql
@@ -1,10 +1,8 @@
-/*Создать общий счет (таблицу buy_pay) на оплату заказа с 
-номером 5. Куда включить номер заказа, количество книг в
- заказе и его общую стоимость. Для решения используйте ОДИН запрос.*/
-CREATE TABLE buy_pay AS
-SELECT buy_id, SUM(buy_book.amount) AS Количество, SUM(price * buy_book.amount) AS Итого
-FROM book
-    INNER JOIN buy_book USING (book_id)
-WHERE buy_id = 5
-GROUP BY buy_id;
-SELECT * FROM buy_pay;
+CREATE TABLE buy_pay AS                         /* создать таблицу как */
+    SELECT                                      /* выбрать данные */
+        buy_id,                                 /* столбец */
+        SUM(buy_book.amount) AS Количество,     /* столбец */
+        SUM(price * buy_book.amount) AS Итого   /* столбец */
+FROM book                                       /* из таблицы */
+    JOIN buy_book USING(book_id)                /* объединенную с таблице (по столбцу) */
+WHERE buy_book.buy_id = 5;                      /* где условие */
