c486870e8efed404faca28db526ced02d418f04e
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-26 21:38:35 +0300

    task 2.5.4.

--- 2. Запросы SQL к связанным таблицам/2.5 База данных «Интернет-магазин книг», запросы корректировки/4.sql
+++ 2. Запросы SQL к связанным таблицам/2.5 База данных «Интернет-магазин книг», запросы корректировки/4.sql
@@ -1,7 +1,8 @@
-/*Количество тех книг на складе, которые были включены в
- заказ с номером 5, уменьшить на то количество, которое в заказе с номером 5  указано.*/
-UPDATE book 
-    INNER JOIN buy_book USING (book_id)
-SET book.amount = book.amount - buy_book.amount
-WHERE book.book_id = buy_book.book_id AND buy_book.buy_id = 5;
-SELECT * FROM book;
+UPDATE                                              /* обновить */
+    book                                            /* таблицу */
+    JOIN buy_book                                   /* объединенную с таблицей */
+    ON buy_book.book_id = book.book_id              /* по столбцу */
+SET                                                 /* заменить */
+    book.amount = book.amount - buy_book.amount     /* количество по формуле */
+WHERE                                               /* где */
+    buy_book.buy_id = 5;                            /* номер заказа = 5 */
