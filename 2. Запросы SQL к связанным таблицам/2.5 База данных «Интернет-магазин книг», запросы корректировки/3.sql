4b3f7ee2d9345f18d510f288a71db3c35defb1eb
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-26 21:14:49 +0300

    task 2.5.3.

--- 2. Запросы SQL к связанным таблицам/2.5 База данных «Интернет-магазин книг», запросы корректировки/3.sql
+++ 2. Запросы SQL к связанным таблицам/2.5 База данных «Интернет-магазин книг», запросы корректировки/3.sql
@@ -1,14 +1,20 @@
-/*В таблицу buy_book добавить заказ с номером 5. Этот
- заказ должен содержать книгу Пастернака «Лирика» в 
- количестве двух экземпляров и книгу Булгакова «Белая гвардия» в одном экземпляре.*/
-INSERT INTO buy_book (buy_id, book_id, amount)
-SELECT 5, book_id, 2
-FROM author INNER JOIN book ON author.author_id = book.author_id
-WHERE name_author = "Пастернак Б.Л." and book.title = "Лирика";
-
-INSERT INTO buy_book (buy_id, book_id, amount)
-SELECT 5, book_id, 1
-FROM author INNER JOIN book ON author.author_id = book.author_id
-WHERE name_author = "Булгаков М.А." and book.title = "Белая гвардия";
-
-SELECT * FROM buy_book;
+/* первая запись */
+INSERT INTO                                                         /* вставить в */
+    buy_book (buy_book.book_id, buy_book.buy_id, buy_book.amount)   /* таблицу (столбцы) */
+SELECT                                                              /* выбрать данные */
+    book_id, 5, 2                                                   /* значения вписываемые в таблицу */
+FROM                                                                /* из */
+    book, author                                                    /* таблицы объединенной с таблицей */
+WHERE                                                               /* где */
+    author.name_author LIKE "Пастернак%"                            /* условие 1 */
+    AND book.title = "Лирика";                                      /* условие 2 */
+/* вторая запись */
+INSERT INTO                                                         /* вставить в */
+    buy_book (buy_book.book_id, buy_book.buy_id, buy_book.amount)   /* таблицу (столбцы) */
+SELECT                                                              /* выбрать данные */
+    book_id, 5, 1                                                   /* значения вписываемые в таблицу */
+FROM                                                                /* из */
+    book, author                                                    /* таблицы объединенной с таблицей */
+WHERE                                                               /* где */
+    author.name_author LIKE "Булгаков%"                             /* условие 1 */
+    AND book.title = "Белая гвардия";                               /* условие 2 */
