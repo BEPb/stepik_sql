5dd8cfbda0b186fb2e8410ee9f870abb6f7ead77
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-29 21:47:26 +0300

    task 3.2.1.

--- 3. Базы данных и SQL запросы/3.2 База данных «Тестирование», запросы корректировки/1.sql
+++ 3. Базы данных и SQL запросы/3.2 База данных «Тестирование», запросы корректировки/1.sql
@@ -1,8 +1,8 @@
-INSERT INTO                                                 /* вставить в */
-    attempt (student_id, subject_id, date_attempt, result)  /* таблицу (столбцы) */
-VALUES (                                                    /* значения */
-    (SELECT student_id FROM student WHERE name_student = "Баранов Павел"),     /* значение */
-    (SELECT subject_id FROM subject WHERE name_subject = "Основы баз данных"), /* значение */
+INSERT INTO                                                                     /* вставить в */
+    attempt (student_id, subject_id, date_attempt, result)                      /* таблицу (столбцы) */
+VALUES (                                                                        /* значения */
+    (SELECT student_id FROM student WHERE name_student = "Баранов Павел"),      /* значение */
+    (SELECT subject_id FROM subject WHERE name_subject = "Основы баз данных"),  /* значение */
     NOW(),                                                                      /* значение */
     NULL                                                                        /* значение */
 );
