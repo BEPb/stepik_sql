144b7225d415b71321eb1350a44c85ceaf8f8639
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-29 20:30:49 +0300

    task 3.1.4.

--- 3. Базы данных и SQL запросы/3.1 База данных «Тестирование», запросы на выборку/4.sql
+++ 3. Базы данных и SQL запросы/3.1 База данных «Тестирование», запросы на выборку/4.sql
@@ -1,17 +1,17 @@
-/*Если студент совершал несколько попыток по одной и той же 
-дисциплине, то вывести разницу в днях между первой и последней 
-попыткой. Информацию вывести по возрастанию разницы. Студентов, 
-сделавших одну попытку по дисциплине, не учитывать. */
-SELECT name_student, ANY_VALUE(name_subject) AS name_subject, ANY_VALUE(DATEDIFF(MAX(date_attempt), MIN(date_attempt))) AS Интервал
-FROM student 
-    INNER JOIN attempt USING (student_id) 
-    INNER JOIN subject USING (subject_id)
-WHERE name_student IN
-    (SELECT ANY_VALUE(name_student)
-    FROM student 
-        INNER JOIN attempt USING (student_id) 
-        INNER JOIN subject USING (subject_id) 
-    GROUP BY name_student 
-    HAVING COUNT(DISTINCT name_subject) = 1)
-GROUP BY name_student
-ORDER BY Интервал;
+SELECT                                                                      /* выбрать данные */
+    name_student,                                                           /* столбец */
+    ANY_VALUE(name_subject) AS name_subject,                                /* столбец */
+    ANY_VALUE(DATEDIFF(MAX(date_attempt), MIN(date_attempt))) AS Интервал   /* столбец */
+FROM student                                                                /* из таблицы */
+    INNER JOIN attempt USING (student_id)                                   /* объединенной с таблицей по стобцу */
+    INNER JOIN subject USING (subject_id)                                   /* объединенной с таблицей по стобцу */
+WHERE name_student IN                                                       /* где имя студента в встроенном запросе */
+    -- встроенный запрос
+    (SELECT ANY_VALUE(name_student)             /* выбрать данные столбец */
+    FROM student                                /* из таблицы */
+        INNER JOIN attempt USING (student_id)   /* объединенной с таблицей по стобцу */
+        INNER JOIN subject USING (subject_id)   /* объединенной с таблицей по стобцу */
+    GROUP BY name_student                       /* сгруппировать по имени студента */
+    HAVING COUNT(DISTINCT name_subject) = 1)    /* количество уникальных значений = 1 */
+GROUP BY name_student                                                       /* сгруппировать по имени */
+ORDER BY Интервал;                                                          /* отсортировать по столбцу */
