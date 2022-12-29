0eb9d2a85090c18a7489e82a3c8eee7cf3d91fa4
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-29 21:06:01 +0300

    task 3.1.8.

--- 3. Базы данных и SQL запросы/3.1 База данных «Тестирование», запросы на выборку/8.sql
+++ 3. Базы данных и SQL запросы/3.1 База данных «Тестирование», запросы на выборку/8.sql
@@ -1,14 +1,12 @@
-/*Посчитать результаты тестирования. Результат попытки вычислить 
-как количество правильных ответов, деленное на 3 (количество 
-вопросов в каждой попытке) и умноженное на 100. Результат округлить
- до двух знаков после запятой. Вывести фамилию студента, название 
- предмета, дату и результат. Информацию отсортировать сначала по фамилии 
- студента, потом по убыванию даты попытки.*/
-SELECT name_student, name_subject, date_attempt, ROUND((SUM(is_correct)/3*100), 2) AS Результат 
-FROM answer 
-    INNER JOIN testing USING (answer_id) 
-    INNER JOIN attempt USING (attempt_id) 
-    INNER JOIN student USING (student_id) 
-    INNER JOIN subject USING (subject_id) 
-GROUP BY date_attempt, name_student, name_subject 
-ORDER BY name_student, date_attempt DESC;
+SELECT                                              /* выбрать данные */
+    name_student,                                   /* столбец */
+    name_subject,                                   /* столбец */
+    date_attempt,                                   /* столбец */
+    ROUND((SUM(is_correct)/3*100), 2) AS Результат  /* столбец */
+FROM answer                                         /* из таблицы */
+    INNER JOIN testing USING (answer_id)            /* объединенной с таблицей по столбцу */
+    INNER JOIN attempt USING (attempt_id)           /* объединенной с таблицей по столбцу */
+    INNER JOIN student USING (student_id)           /* объединенной с таблицей по столбцу */
+    INNER JOIN subject USING (subject_id)           /* объединенной с таблицей по столбцу */
+GROUP BY date_attempt, name_student, name_subject   /* сгруппировать по столбцам */
+ORDER BY name_student, date_attempt DESC;           /* отсортировать по имени, затем по дате в обратной последовательности */
