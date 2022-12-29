5c79a8e201405680cb36a98c28ea1f3885987c31
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-29 19:58:41 +0300

    task 3.1.2.

--- 3. Базы данных и SQL запросы/3.1 База данных «Тестирование», запросы на выборку/2.sql
+++ 3. Базы данных и SQL запросы/3.1 База данных «Тестирование», запросы на выборку/2.sql
@@ -1,8 +1,11 @@
-/*Вывести студента (различных студентов), имеющих максимальные 
-результаты попыток. Информацию отсортировать в алфавитном порядке по фамилии студента.*/
-SELECT name_student, result 
-FROM student 
-    INNER JOIN attempt USING (student_id) 
-WHERE result = 
-    (SELECT MAX(result) FROM attempt) 
-ORDER BY name_student;
+SELECT                                          /* выбрать данные */
+    subject.name_subject,                       /* таблица.столбец */
+    COUNT(attempt.attempt_id) AS Количество,    /* столбец количество */
+    ROUND(AVG(attempt.result), 2) AS Среднее    /* столбец среднее */
+FROM                                            /* из */
+    attempt                                     /* таблицы */
+    RIGHT JOIN subject USING(subject_id)        /* объединенной с таблицей по столбцу */
+GROUP BY                                        /* сгруппировать */
+    subject_id                                  /* по столбцу */
+ORDER BY                                        /* отсортировать */
+    Среднее DESC;                               /* по столбцу среднее в обратном порядке */
