09f8fc01b443378a5a4000990c5a4ba7310ecd48
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 16:08:04 +0300

    task 3.4.1.

--- 3. Базы данных и SQL запросы/3.4 База данных «Абитуриент», запросы корректировки/1.sql
+++ 3. Базы данных и SQL запросы/3.4 База данных «Абитуриент», запросы корректировки/1.sql
@@ -1,16 +1,14 @@
-/*Создать вспомогательную таблицу applicant,  куда включить id 
-образовательной программы, id абитуриента, сумму баллов абитуриентов 
-в отсортированном сначала по id образовательной программы, а потом 
-по убыванию суммы баллов виде (использовать запрос из предыдущего урока).*/
-CREATE TABLE applicant AS
-(SELECT program_enrollee.program_id, enrollee.enrollee_id, SUM(result) AS itog
-FROM enrollee
-    INNER JOIN program_enrollee ON enrollee.enrollee_id = program_enrollee.enrollee_id
-    INNER JOIN program ON program_enrollee.program_id = program.program_id
-    INNER JOIN program_subject ON program.program_id = program_subject.program_id
-    INNER JOIN subject ON program_subject.subject_id = subject.subject_id
-    INNER JOIN enrollee_subject ON subject.subject_id = enrollee_subject.subject_id 
-        and enrollee_subject.enrollee_id = enrollee.enrollee_id
-GROUP BY program_enrollee.program_id, enrollee.enrollee_id
-ORDER BY program_enrollee.program_id, itog DESC);
-SELECT * FROM applicant;
+CREATE TABLE applicant                                      /* создать таблицу */
+SELECT                                                      /* выбрать данные */
+    program_id,                                             /* столбец */
+    enrollee.enrollee_id,                                   /* столбец */
+    SUM(result) AS itog                                     /* столбец */
+FROM enrollee                                               /* из таблицы */
+     JOIN program_enrollee USING(enrollee_id)               /* объединенной с таблицей по столбцу */
+     JOIN program USING(program_id)                         /* объединенной с таблицей по столбцу */
+     JOIN program_subject USING(program_id)                 /* объединенной с таблицей по столбцу */
+     JOIN subject USING(subject_id)                         /* объединенной с таблицей по столбцу */
+     JOIN enrollee_subject USING(subject_id)                /* объединенной с таблицей по столбцу */
+WHERE enrollee_subject.enrollee_id = enrollee.enrollee_id   /* где условие */
+GROUP BY program_id, enrollee_id                            /* сгруппировать по столбцам */
+ORDER BY program_id, itog DESC;                             /* отсортировать по столбцам */
