d64ea7bb6b7c27753b14b5adcdd525346740ae0a
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 15:25:18 +0300

    task 3.3.6.

--- 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/6.sql
+++ 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/6.sql
@@ -1,8 +1,8 @@
-/*Посчитать, сколько дополнительных баллов получит каждый абитуриент. 
-Информацию вывести в отсортированном по фамилиям виде.*/
-SELECT name_enrollee, IF(SUM(bonus) IS NULL, 0, SUM(bonus)) AS Бонус 
-FROM achievement
-    INNER JOIN enrollee_achievement USING (achievement_id)
-    RIGHT JOIN enrollee USING (enrollee_id)
-GROUP BY enrollee_id
-ORDER BY name_enrollee;
+SELECT                                                      /* выбрать данные */
+    name_enrollee,                                          /* столбец */
+    IF(SUM(bonus) IS NULL, 0, SUM(bonus)) AS Бонус          /* столбец */
+FROM achievement                                            /* из таблицы */
+    INNER JOIN enrollee_achievement USING (achievement_id)  /* объединенной с таблицей по столбцу */
+    RIGHT JOIN enrollee USING (enrollee_id)                 /* объединенной с таблицей по столбцу */
+GROUP BY enrollee_id                                        /* сгруппировать по столбцу */
+ORDER BY name_enrollee;                                     /* отсортировать по столбцу */
