ab5b2277ff5b9d7bca77686e92d976c72331d514
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 15:18:47 +0300

    task 3.3.4.

--- 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/4.sql
+++ 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/4.sql
@@ -1,10 +1,7 @@
-/*Вывести образовательные программы, для которых минимальный 
-балл ЕГЭ по каждому предмету больше или равен 40 баллам. 
-Программы вывести в отсортированном по алфавиту виде.*/
-SELECT DISTINCT ANY_VALUE(name_program) AS name_program
-FROM program 
-    INNER JOIN program_subject USING (program_id)
-GROUP BY name_program
-HAVING MIN(min_result) >= 40
-ORDER BY name_program
-;
+SELECT DISTINCT                                     /* выбрать уникальные значения */
+    ANY_VALUE(name_program) AS name_program         /* столбца */
+FROM program                                        /* из таблицы */
+    INNER JOIN program_subject USING (program_id)   /* объединенной с таблицей по столбцу */
+GROUP BY name_program                               /* сгруппировать по столбцу */
+HAVING MIN(min_result) >= 40                        /* условие */
+ORDER BY name_program;                              /* отсортировать по столбцу */
