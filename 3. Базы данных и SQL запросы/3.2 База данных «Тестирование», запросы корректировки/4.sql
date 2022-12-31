f14afaa0fb8c89b45e3953c1254004ac36c5e624
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 14:53:43 +0300

    task 3.2.4.

--- 3. Базы данных и SQL запросы/3.2 База данных «Тестирование», запросы корректировки/4.sql
+++ 3. Базы данных и SQL запросы/3.2 База данных «Тестирование», запросы корректировки/4.sql
@@ -1,6 +1,2 @@
-/*Удалить из таблицы attempt все попытки, выполненные раньше 1 мая 
-2020 года. Также удалить и все соответствующие этим попыткам вопросы из таблицы testing*/
-DELETE FROM attempt
-WHERE date_attempt < "2020-05-01";
-SELECT * FROM attempt;
-SELECT * FROM testing;
+DELETE FROM attempt                 /* удалить данные из таблицы */
+WHERE date_attempt < "2020-05-01";  /* где значения столбца меньше чем */
