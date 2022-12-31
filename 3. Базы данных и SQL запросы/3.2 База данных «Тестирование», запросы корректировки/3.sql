eedd84087373f0f047ee61d675697b6568de35fe
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 14:51:13 +0300

    task 3.2.3.

--- 3. Базы данных и SQL запросы/3.2 База данных «Тестирование», запросы корректировки/3.sql
+++ 3. Базы данных и SQL запросы/3.2 База данных «Тестирование», запросы корректировки/3.sql
@@ -1,12 +1,7 @@
-/*Студент прошел тестирование (то есть все его ответы занесены в 
-таблицу testing), далее необходимо вычислить результат(запрос) и 
-занести его в таблицу attempt для соответствующей попытки.  
-Результат попытки вычислить как количество правильных ответов, 
-деленное на 3 (количество вопросов в каждой попытке) и умноженное на 
-100. Результат округлить до целого.*/
-UPDATE attempt SET result = 
-    (SELECT CEILING(SUM(is_correct) / 3 * 100)
-    FROM testing
-        INNER JOIN answer USING (answer_id)
-    WHERE attempt_id = 8)
-WHERE attempt_id = 8;
+UPDATE attempt                                  /* обновить таблицу */
+SET result =                                    /* значение = встроенный запрос */
+    (SELECT CEILING(SUM(is_correct) / 3 * 100)  /* выбрать данные вычисления */
+    FROM testing                                /* из таблицы */
+        INNER JOIN answer USING (answer_id)     /* объединенной с таблицей */
+    WHERE attempt_id = 8)                       /* где условие */
+WHERE attempt_id = 8;                           /* где условие */
