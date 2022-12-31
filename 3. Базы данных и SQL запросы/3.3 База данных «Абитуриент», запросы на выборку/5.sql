16e9ae9b38e35cccca589b635aa3667343477487
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 15:21:45 +0300

    task 3.3.5.

--- 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/5.sql
+++ 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/5.sql
@@ -1,3 +1,4 @@
-/*Вывести образовательные программы, которые имеют самый 
-большой план набора,  вместе с этой величиной.*/
-SELECT name_program, plan FROM program WHERE plan = (SELECT MAX(plan) FROM program);
+SELECT name_program, plan               /* выбрать данные столбцы */
+FROM program                            /* из таблицы */
+WHERE plan =                            /* где условие = встроенному запросу */
+    (SELECT MAX(plan) FROM program);    /* встроенный запрос */
