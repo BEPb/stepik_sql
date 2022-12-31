85e64028fd31533cae51dfe5e4f290b596e43de1
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 15:15:30 +0300

    task 3.3.3.

--- 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/3.sql
+++ 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/3.sql
@@ -1,9 +1,10 @@
-/*Выведите количество абитуриентов, сдавших ЕГЭ по каждому предмету, 
-максимальное, минимальное и среднее значение баллов по предмету ЕГЭ. 
-Информацию отсортировать по названию предмета в алфавитном порядке, 
-среднее значение округлить до одного знака после запятой.*/
-SELECT ANY_VALUE(name_subject) AS name_subject, COUNT(name_subject) AS Количество, MAX(result) AS Максимум, MIN(result) AS Минимум, ROUND(AVG(result), 1) AS Среднее
-FROM subject
-    INNER JOIN enrollee_subject USING (subject_id)
-GROUP BY subject_id
-ORDER BY name_subject;
+SELECT                                              /* выбрать данные */
+    ANY_VALUE(name_subject) AS name_subject,        /* столбец */
+    COUNT(name_subject) AS Количество,              /* столбец */
+    MAX(result) AS Максимум,                        /* столбец */
+    MIN(result) AS Минимум,                         /* столбец */
+    ROUND(AVG(result), 1) AS Среднее                /* столбец */
+FROM subject                                        /* из таблицы */
+    INNER JOIN enrollee_subject USING (subject_id)  /* объединенной с таблицей по столбцу */
+GROUP BY subject_id                                 /* сгруппировать по столбцу */
+ORDER BY name_subject;                              /* вывести результат по столбцу */
