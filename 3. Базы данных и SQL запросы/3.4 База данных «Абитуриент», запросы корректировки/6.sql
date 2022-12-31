c009dc4ae5cbcda336bc4127490b96eae34bd4c7
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 16:54:16 +0300

    task 3.4.6.

--- 3. Базы данных и SQL запросы/3.4 База данных «Абитуриент», запросы корректировки/6.sql
+++ 3. Базы данных и SQL запросы/3.4 База данных «Абитуриент», запросы корректировки/6.sql
@@ -1,9 +1,12 @@
 /*Занести в столбец str_id таблицы applicant_order 
 нумерацию абитуриентов, которая начинается с 1 для каждой образовательной программы.*/
-SET @num_pr = 0;
-SET @row_num = 1;
-UPDATE applicant_order 
-    INNER JOIN (SELECT *, if(program_id = @num_pr, @row_num := @row_num + 1, @row_num := 1) AS str_num, @num_pr := program_id AS add_var from applicant_order) AS buff
-    ON applicant_order.program_id = buff.program_id AND applicant_order.enrollee_id = buff.enrollee_id
-SET applicant_order.str_id = buff.str_num;
-SELECT * FROM applicant_order;
+
+-- задаем переменные
+SET @num_pr := 1;
+SET @row_num := 0;
+
+-- создаем запрос
+UPDATE applicant_order  /* обновить таблицу */
+    SET str_id = if(program_id = @num_pr,                       /* если условие 1 истина */
+                    @row_num := @row_num + 1,                   /* то заменить на значение */
+                    @row_num := 1 AND @num_pr := @num_pr + 1);  /* иначе заменить на */
