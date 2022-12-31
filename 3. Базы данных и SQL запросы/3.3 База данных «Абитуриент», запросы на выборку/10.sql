849f6680afac8f3f1e4b15d353b11dd033531bec
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 15:49:04 +0300

    task 3.3.10.

--- 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/10.sql
+++ 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/10.sql
@@ -1,17 +1,12 @@
-/*Вывести название образовательной программы и фамилию тех 
-абитуриентов, которые подавали документы на эту образовательную 
-программу, но не могут быть зачислены на нее. Эти абитуриенты имеют
- результат по одному или нескольким предметам ЕГЭ, необходимым 
- для поступления на эту образовательную программу, меньше 
- минимального балла. Информацию вывести в отсортированном 
- сначала по программам, а потом по фамилиям абитуриентов виде.*/
-SELECT DISTINCT name_program, name_enrollee
-FROM enrollee
-    INNER JOIN program_enrollee ON enrollee.enrollee_id = program_enrollee.enrollee_id
-    INNER JOIN program ON program_enrollee.program_id = program.program_id
-    INNER JOIN program_subject ON program.program_id = program_subject.program_id
-    INNER JOIN subject ON program_subject.subject_id = subject.subject_id
-    INNER JOIN enrollee_subject ON subject.subject_id = enrollee_subject.subject_id 
-        and enrollee_subject.enrollee_id = enrollee.enrollee_id
-WHERE result < min_result
-ORDER BY name_program, name_enrollee;
+SELECT                                          /* выбрать данные */
+    DISTINCT name_program,                      /* уникальные значения столбца */
+    name_enrollee                               /* столбец */
+FROM enrollee                                   /* из таблицы */
+    JOIN program_enrollee USING(enrollee_id)    /* объединенной с таблице по столбцу */
+    JOIN program USING(program_id)              /* объединенной с таблице по столбцу */
+    JOIN program_subject USING(program_id)      /* объединенной с таблице по столбцу */
+    JOIN subject USING(subject_id)              /* объединенной с таблице по столбцу */
+    JOIN enrollee_subject ON subject.subject_id = enrollee_subject.subject_id   /* объединенной с таблице по столбцу и */
+        and enrollee_subject.enrollee_id = enrollee.enrollee_id                 /* столбцу */
+WHERE result < min_result                       /* где условие */
+ORDER BY name_program, name_enrollee;           /* сгруппировать по столбцам */
