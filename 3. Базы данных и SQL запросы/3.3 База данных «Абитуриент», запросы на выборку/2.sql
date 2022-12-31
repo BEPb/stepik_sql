ef772c8bfd423b10cf6e3e15884d76ba19b6fc33
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 15:11:51 +0300

    task 3.3.2.

--- 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/2.sql
+++ 3. Базы данных и SQL запросы/3.3 База данных «Абитуриент», запросы на выборку/2.sql
@@ -1,6 +1,5 @@
-/*Вывести образовательные программы, на которые для поступления необходим предмет «Информатика».*/
-SELECT name_program 
-FROM program 
-    INNER JOIN program_subject USING (program_id)
-    INNER JOIN subject USING (subject_id)
-WHERE name_subject = "Информатика";
+SELECT name_program                                 /* выбрать данные столбца */
+FROM program                                        /* из таблицы */
+    INNER JOIN program_subject USING (program_id)   /* объединенной с таблицей по столбцу */
+    INNER JOIN subject USING (subject_id)           /* объединенной с таблицей по столбцу */
+WHERE name_subject = "Информатика";                 /* где условие */
