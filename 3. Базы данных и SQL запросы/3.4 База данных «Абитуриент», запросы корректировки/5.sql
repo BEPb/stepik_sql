1d4b763df0368567358a8d96d2291a435dae9ff0
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2022-12-31 16:38:48 +0300

    task 3.4.5.

--- 3. Базы данных и SQL запросы/3.4 База данных «Абитуриент», запросы корректировки/5.sql
+++ 3. Базы данных и SQL запросы/3.4 База данных «Абитуриент», запросы корректировки/5.sql
@@ -1,3 +1,2 @@
-/*Включить в таблицу applicant_order новый столбец str_id 
-целого типа , расположить его перед первым.*/
-ALTER TABLE applicant_order ADD str_id INTEGER FIRST;
+ALTER TABLE applicant_order     /* включить в таблицу  */
+    ADD str_id INTEGER FIRST;   /* столбец целого типа , расположить его перед первым */
